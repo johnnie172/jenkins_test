ec22afcb56f2e6f700f8729369eefe68040c0fbf
johnnie172 <64689498+johnnie172@users.noreply.github.com>
2020-09-09 17:41:06 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,3 +6,18 @@ class TestUtilities(unittest.TestCase):
 	
 	def test_helloworld(self):
 		self.assertEqual(print_hello_world(), "Hello World")
+
+
+def suite():
+
+    test_suite = unittest.TestSuite()
+    test_suite.addTest(unittest.makeSuite(TestUtilities))
+
+
+    return test_suite
+
+mySuit=suite()
+
+if __name__ == '__main__':
+    runner=unittest.TextTestRunner()
+    runner.run(mySuit)
